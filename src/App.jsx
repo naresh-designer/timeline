13bbcfe09db49bd5c5b736876e4223739e78e016
Naresh Kumar <nareshcrazyaccess@gmail.com>
2024-10-18 21:45:26 +0530

    timeline

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import TimeLine from "./TimeLine"
+
+
+const App = () => {
+  return (
+    <main>
+      <div className="wrapper">
+      <TimeLine/>
+      </div>
+    </main>
+  )
+}
+
+export default App
---
+++ src/TimeLine.jsx
@@ -0,0 +1,54 @@
+
+
+const TimeLine = () => {
+  return (
+    <main>
+        <div className="timeline">
+            <div className="timeline__container">
+            <div className="round">1</div>
+                <div className="timeline__text">
+                    <h1>Home 1</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem voluptatem mollitia quaerat ab optio voluptates quis aliquam, hic commodi perspiciatis.</p>
+                </div>
+            </div>
+            <div className="timeline__container">
+            <div className="round">2</div>
+                <div className="timeline__text">
+                    <h1>Home 2</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem voluptatem mollitia quaerat ab optio voluptates quis aliquam, hic commodi perspiciatis.</p>
+                </div>
+            </div>
+            <div className="timeline__container">
+            <div className="round">3</div>
+                <div className="timeline__text">
+                    <h1>Home 3</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem voluptatem mollitia quaerat ab optio voluptates quis aliquam, hic commodi perspiciatis.</p>
+                </div>
+            </div>
+            <div className="timeline__container">
+            <div className="round">4</div>
+                <div className="timeline__text">
+                    <h1>Home 4</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem voluptatem mollitia quaerat ab optio voluptates quis aliquam, hic commodi perspiciatis.</p>
+                </div>
+            </div>
+            <div className="timeline__container">
+            <div className="round">4</div>
+                <div className="timeline__text">
+                    <h1>Home 4</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem voluptatem mollitia quaerat ab optio voluptates quis aliquam, hic commodi perspiciatis.</p>
+                </div>
+            </div>
+            <div className="timeline__container">
+            <div className="round">4</div>
+                <div className="timeline__text">
+                    <h1>Home 4</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem voluptatem mollitia quaerat ab optio voluptates quis aliquam, hic commodi perspiciatis.</p>
+                </div>
+            </div>
+        </div>
+    </main>
+  )
+}
+
+export default TimeLine
